4cd727b4cad9605d23543ffbdfa686d690b018ef
guilhermedebrites <guilhermedebrites1@gmail.com>
2025-06-08 11:34:54 -0300

    feat: changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['date-fns', 'react', 'react-dom'],
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
   build: {
-    target: 'es2020',
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      include: [/node_modules/],
-    },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor_react'
-            if (id.includes('date-fns')) return 'vendor_datefns'
-            return 'vendor'
-          }
-        },
-      },
-    },
+    minify: false,
   },
 })
